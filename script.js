7d8aac91d8fd3e81bb0ffb1b84f7faab18ad4541
Lucas Carvalho <lucascarvalhoo@outlook.com>
2025-12-30 22:12:22 -0300

    Funciona como uma exibição de slides, comecei um prototipo de transição

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const locais = [
-    {titulo: "Egito", descricao: "As Pirâmides de Gizé estão localizadas nos arredores do Cairo e são um dos maiores símbolos da civilização egípcia antiga. Construídas há cerca de 4.500 anos, durante a IV Dinastia, elas serviam como túmulos para os faraós Quéops, Quéfren e Miquerinos. A maior delas, a Pirâmide de Quéops, é uma das Sete Maravilhas do Mundo Antigo e a única que ainda existe. Essas estruturas impressionam pela precisão matemática, pelo alinhamento astronômico e pelo enorme esforço humano envolvido em sua construção."},
-    {titulo: "O Coliseu", descricao: "O Coliseu de Roma, também conhecido como Anfiteatro Flaviano, foi inaugurado no ano 80 d.C. e é um dos maiores exemplos da engenharia romana. Com capacidade para cerca de 50 mil espectadores, o Coliseu era usado para combates de gladiadores, execuções públicas e encenações de batalhas. Sua estrutura em arcos e concreto influenciou profundamente a arquitetura ocidental. Atualmente, é um dos monumentos mais emblemáticos da Roma Antiga e um importante símbolo histórico e cultural da Itália."},
-    {titulo: "Stonehenge", descricao: "Stonehenge é um monumento megalítico localizado no sul da Inglaterra, construído entre aproximadamente 3000 e 2000 a.C.. Ele é formado por grandes blocos de pedra dispostos em círculos concêntricos. Embora sua função exata ainda seja debatida, acredita-se que Stonehenge tenha sido usado para rituais religiosos, cerimônias funerárias e observações astronômicas, especialmente relacionadas aos solstícios. O transporte e a organização das pedras demonstram um notável conhecimento técnico das sociedades pré-históricas."},
-    {titulo: " Machu Picchu", descricao: "Machu Picchu é uma antiga cidade inca situada no topo das montanhas dos Andes peruanos, a cerca de 2.400 metros de altitude. Construída no século XV, durante o reinado do imperador Pachacuti, a cidade provavelmente teve funções religiosas, políticas e astronômicas. O local é famoso por sua arquitetura em pedra perfeitamente encaixada, seus terraços agrícolas e por sua integração harmoniosa com a paisagem natural. Redescoberta em 1911, Machu Picchu é hoje um dos sítios arqueológicos mais visitados do mundo."}
+    {titulo: "Egito", img: "egito.jpg",descricao: "As Pirâmides de Gizé estão localizadas nos arredores do Cairo e são um dos maiores símbolos da civilização egípcia antiga. Construídas há cerca de 4.500 anos, durante a IV Dinastia, elas serviam como túmulos para os faraós Quéops, Quéfren e Miquerinos. A maior delas, a Pirâmide de Quéops, é uma das Sete Maravilhas do Mundo Antigo e a única que ainda existe. Essas estruturas impressionam pela precisão matemática, pelo alinhamento astronômico e pelo enorme esforço humano envolvido em sua construção."},
+    {titulo: "O Coliseu", img: "roma.avif", descricao: "O Coliseu de Roma, também conhecido como Anfiteatro Flaviano, foi inaugurado no ano 80 d.C. e é um dos maiores exemplos da engenharia romana. Com capacidade para cerca de 50 mil espectadores, o Coliseu era usado para combates de gladiadores, execuções públicas e encenações de batalhas. Sua estrutura em arcos e concreto influenciou profundamente a arquitetura ocidental. Atualmente, é um dos monumentos mais emblemáticos da Roma Antiga e um importante símbolo histórico e cultural da Itália."},
+    {titulo: "Stonehenge", img: "Stonehenge.webp", descricao: "Stonehenge é um monumento megalítico localizado no sul da Inglaterra, construído entre aproximadamente 3000 e 2000 a.C.. Ele é formado por grandes blocos de pedra dispostos em círculos concêntricos. Embora sua função exata ainda seja debatida, acredita-se que Stonehenge tenha sido usado para rituais religiosos, cerimônias funerárias e observações astronômicas, especialmente relacionadas aos solstícios. O transporte e a organização das pedras demonstram um notável conhecimento técnico das sociedades pré-históricas."},
+    {titulo: " Machu Picchu", img: "machu-picchu.jpg", descricao: "Machu Picchu é uma antiga cidade inca situada no topo das montanhas dos Andes peruanos, a cerca de 2.400 metros de altitude. Construída no século XV, durante o reinado do imperador Pachacuti, a cidade provavelmente teve funções religiosas, políticas e astronômicas. O local é famoso por sua arquitetura em pedra perfeitamente encaixada, seus terraços agrícolas e por sua integração harmoniosa com a paisagem natural. Redescoberta em 1911, Machu Picchu é hoje um dos sítios arqueológicos mais visitados do mundo."}
 ]
 let frente = document.getElementById('frente')
 let tras = document.getElementById('tras')
@@ -12,11 +12,17 @@ var i = 0;
     tras.onclick = () => mudar_indice(tras);
 
 function mudar_indice(sentido){
-    if(sentido == frente && i <locais.length){
+    if(sentido == frente && i < locais.length - 1){
         i++;
     }
+    else if(sentido == frente && i == locais.length - 1){
+        i = 0;
+    }
     else if(sentido == tras && i > 0){
-        i--
+        i--;
+    }
+    else if(sentido == tras && i == 0){
+        i = locais.length - 1;
     }
     mudar_local();
 }
@@ -25,4 +31,17 @@ function mudar_local(){
        let descricao = document.querySelector(".descricao");
        titulo.textContent = locais[i].titulo;
        descricao.textContent = locais[i].descricao;
+       document.body.style.backgroundImage = `url(${locais[i].img})`;
+
+}
+function abririmg(img){
+    const aumentar = document.getElementById("crescer_img");
+    const maior = document.getElementById("imagem_maior");
+
+    maior.src = img.src;
+    aumentar.style.display = "flex";
+}
+function fecharimg(){
+    const aumentar = document.getElementById("crescer_img");
+    aumentar.style.display = "none";
 }
